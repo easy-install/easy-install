e1af039520ef0937615d6dddca30c851d2d0e9d1
ahaoboy <504595380@qq.com>
2024-11-28 12:18:35 +0800

    add macos test

--- src/install.rs
+++ src/install.rs
@@ -432,13 +432,13 @@ mod test {
 
     #[tokio::test]
     async fn test_get_artifact_url() {
-        let repo = Repo::try_from("https://github.com/ahaoboy/ansi2/releases/tag/v0.2.11").unwrap();
+        let repo = Repo::try_from("https://github.com/axodotdev/cargo-dist").unwrap();
         let url = repo.get_artifact_url().await[0].clone();
         let fmt = PkgFmt::guess_pkg_format(&url).unwrap();
         let files = download(&url).await;
         let out_dir = tempdir().unwrap();
         let files = files.and_extract(fmt, out_dir.path()).await.unwrap();
-        assert!(files.has_file(Path::new(if IS_WINDOWS { "ansi2.exe" } else { "ansi2" })));
+        assert!(files.has_file(Path::new(if IS_WINDOWS { "dist.exe" } else { "dist" })));
     }
 
     #[tokio::test]
