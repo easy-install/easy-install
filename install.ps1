2e361befa0b4eed670f645bc224ba8e50631f910
ahaoboy <504595380@qq.com>
2025-01-12 22:25:25 +0800

    fix install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,10 @@ $FILENAME = "ei-x86_64-pc-windows-gnu.zip"
 $INSTALL_DIR = Resolve-Path "~/easy-install"
 
 function Ensure-ContainingDirExists {
-  New-Item -Path $INSTALL_DIR -ItemType Directory -Force | Out-Null
+  if (-not (Test-Path -Path $INSTALL_DIR)) {
+    New-Item -Path $INSTALL_DIR -ItemType Directory -Force | Out-Null
+  }
+
   $currentPath = [Environment]::GetEnvironmentVariable('Path', 'User')
   if (-not ($currentPath -split ';' | Where-Object { $_ -eq $INSTALL_DIR })) {
       [Environment]::SetEnvironmentVariable('Path', "$currentPath;$INSTALL_DIR", 'User')
@@ -19,21 +22,23 @@ function Download {
         $URL = "https://github.com/ahaoboy/easy-install/releases/download/$RELEASE/$FILENAME"
     }
 
-    $tempFile = [System.IO.Path]::GetTempFileName()
-    Remove-Item -Path $tempFile
+    $tempFile = [System.IO.Path]::GetTempFileName()+(Get-Random)
+    if (Test-Path -Path $tempFile) {
+      Remove-Item -Path $tempFile -Force -Recurse
+    }
     $DOWNLOAD_DIR=New-Item -ItemType Directory -Path $tempFile
 
     Write-Host "Downloading $URL..."
 
     try {
-        Invoke-WebRequest -Uri $URL -OutFile "$DOWNLOAD_DIR\$FILENAME" -UseBasicP -ErrorAction Stop
+        Invoke-WebRequest -Uri $URL -OutFile "$DOWNLOAD_DIR/$FILENAME" -UseBasicP -ErrorAction Stop
     } catch {
         Write-Host "Download failed. Check that the release/filename are correct."
         exit 1
     }
 
     Expand-Archive -Path "$DOWNLOAD_DIR\$FILENAME" -DestinationPath $DOWNLOAD_DIR -Force
-    Move-Item -Path "$DOWNLOAD_DIR/ei.exe" -Destination "$INSTALL_DIR/ei.exe" -Force
+    Move-Item -Path "$DOWNLOAD_DIR\ei.exe" -Destination "$INSTALL_DIR\ei.exe" -Force
     Write-Host "Successfully installed to $INSTALL_DIR/ei.exe"
 }
 
