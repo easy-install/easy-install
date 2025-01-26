b08735af3eed5fcee94caf42d874b68ef0e8ed3d
ahaoboy <504595380@qq.com>
2025-01-26 11:58:10 +0800

    ci: add github path

--- src/env.rs
+++ src/env.rs
@@ -1,41 +1,20 @@
 use std::path::PathBuf;
 
+use crud_path::{add_github_path, is_github};
+
 pub const IS_WINDOWS: bool = cfg!(target_os = "windows");
 
-fn is_github_action() -> bool {
-    std::env::var("GITHUB_ACTIONS") == Ok("true".to_string())
-}
-#[cfg(not(target_os = "windows"))]
-fn add_github_path(path: &str) {
-    std::process::Command::new("bash")
-        .args(["-c", &format!(r#"echo "PATH=$PATH:{path}" >> $GITHUB_ENV"#)])
-        .output()
-        .expect("add_github_path error");
-}
-
-#[cfg(target_os = "windows")]
-fn add_github_path(path: &str) {
-    std::process::Command::new("powershell")
-        .args([
-            "-c",
-            &format!(
-                r#"echo "{path}" | Out-File -Append -FilePath $env:GITHUB_PATH -Encoding utf8"#
-            ),
-        ])
-        .output()
-        .expect("add_github_path error");
-}
-
 pub fn add_to_path(dir: &str) {
-    if is_github_action() {
-        add_github_path(dir);
-    }
-
     if crud_path::has_path(dir) {
         return;
     }
-    if crud_path::add_path(dir) {
-        println!("Successfully added {dir} to $PATH");
+
+    if is_github() {
+        add_github_path(dir);
+    }
+
+    if let Some(sh) = crud_path::add_path(dir) {
+        println!("Successfully added {dir} to {sh}'s $PATH");
     } else {
         println!("You need to add {dir} to your PATH");
     }
