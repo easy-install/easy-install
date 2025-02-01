3ea7c7cdbf56f92884bab34d1c643a7f85e48eba
ahaoboy <504595380@qq.com>
2025-02-01 21:39:10 +0800

    fix ci

--- ts/tool.ts
+++ ts/tool.ts
@@ -282,6 +282,14 @@ export function addExecutePermission(filePath: string) {
 }
 
 export function atomiInstall(src: string, dst: string) {
+  const dir = path.dirname(dst)
+  if (fs.existsSync(dir) && fs.statSync(dir).isFile()) {
+    console.log(`delete ${dir}`)
+    fs.rmSync(dir)
+  }
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+  }
   fs.copyFileSync(src, dst)
 }
 
