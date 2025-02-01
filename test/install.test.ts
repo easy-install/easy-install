1d9fffbdc459eb09fc3060b3cd6d577d8b9bae18
ahaoboy <504595380@qq.com>
2025-02-01 12:10:07 +0800

    ts: install return array

--- test/install.test.ts
+++ test/install.test.ts
@@ -9,17 +9,17 @@ test('fileInstall', async () => {
     'https://github.com/quickjs-ng/quickjs/releases/latest/download/qjs-linux-x86_64'
   const name = 'qjs'
   const output = await fileInstall({ url, name })
-  expect(existsSync(output?.installPath!)).toEqual(true)
+  expect(existsSync(output[0]?.installPath!)).toEqual(true)
 
   const output2 = await fileInstall({ url, name }, undefined, 'test-install')
-  expect(existsSync(output2?.installPath!)).toEqual(true)
+  expect(existsSync(output2[0]?.installPath!)).toEqual(true)
 })
 
 test('artifactInstall', async () => {
   const url =
     'https://github.com/ahaoboy/mujs-build/releases/download/v0.0.1/mujs-x86_64-unknown-linux-gnu.tar.gz'
   const output = await artifactInstall(url)!
-  const mujsPath = join(output?.installDir!, 'mujs')
+  const mujsPath = join(output[0]?.installDir!, 'mujs')
   expect(existsSync(mujsPath)).toEqual(true)
 })
 
@@ -27,6 +27,6 @@ test('artifactInstall ', async () => {
   const url =
     'https://github.com/ahaoboy/mujs-build/releases/download/v0.0.1/mujs-x86_64-unknown-linux-gnu.tar.gz'
   const output = await artifactInstall(url)!
-  const mujsPath = join(output?.installDir!, 'mujs')
+  const mujsPath = join(output[0]?.installDir!, 'mujs')
   expect(existsSync(mujsPath)).toEqual(true)
 })
